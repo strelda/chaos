3682a374d5768adac351043096c148b2ccea821e
strelda <strelecekja@gmail.com>
2023-03-28 11:26:05 +0200

    fix

---
+++ quantum_chaos/plots/ps.gnuplot
@@ -0,0 +1,16 @@
+set terminal pngcairo
+set output 'plots/ps.png'
+
+set xlabel "S"
+set ylabel "Frequency"
+set grid
+set style fill solid 1 noborder
+
+bin_width = 100
+bin_number(x) = floor(x/bin_width)
+rounded(x) = bin_width * (bin_number(x) + 0.5)
+set boxwidth bin_width
+# set logscale x
+# set xtics ("0" 0, "2·10^3" 2e3, "4·10^3" 4e3, "6·10^3" 6e3, "8·10^3" 8e3, "1·10^4" 1e4, "1.2·10^3" 1.2*1e4, "1.4·10^4" 1.4*1e4)
+
+plot 'generated_data/ps.txt' using (rounded($1)):(1.0) smooth freq with boxes notitle
--- quantum_chaos/src/main.rs
+++ quantum_chaos/src/main.rs
@@ -10,28 +10,34 @@ fn calculate_well_energies(a: f64, b: f64, emax: f64) -> Result<(), std::io::Err
     let mut file = std::fs::File::create("generated_data/well_energies.txt")?;
 
     while energy < emax {
+        energy = 0.0;
         while energy < emax {
             energy = well_energy(n1, n2, a, b);
             writeln!(file, "{}", energy)?;
             n2 += 1;
         }
+        n2 = 1;
         n1 += 1;
     }
     Ok(())
 }
+
 fn calculate_well_energy_differences(a: f64, b: f64, emax: f64) -> Result<(), std::io::Error> {
     let mut n1: i64 = 1;
     let mut n2: i64 = 1;
     let mut energy1: f64 = 0.;
     let mut energy2: f64 = 0.;
-    let mut file = std::fs::File::create("generated_data/well_energy_differences.txt")?;
+    let mut file = std::fs::File::create("generated_data/ps.txt")?;
 
     while energy1 < emax {
         energy1 = well_energy(n1, n1, a, b);
+        n2 = n1;
+        energy2 = 0.;
         while energy2 < emax {
-            energy2 = well_energy(n1, n2+1, a, b);
-            let energy_dif = energy2 - energy1;
-            writeln!(file, "{}", energy_dif)?;
+            energy2 = well_energy(n1, n2 + 1, a, b);
+            let s: f64 = energy2 - energy1;
+
+            writeln!(file, "{}", s)?;
             n2 += 1;
             energy1 = energy2;
         }
@@ -41,12 +47,14 @@ fn calculate_well_energy_differences(a: f64, b: f64, emax: f64) -> Result<(), st
 }
 
 
+
+
 fn main() {
     // set well parameters
     let a: f64 = (PI / 3.0).sqrt();
     let b: f64 = 1.0;
     // set the criteria for the energy histogram
-    let emax: f64 = 1e7;
+    let emax: f64 = 1e6;
 
     // generate the energy histogram data
     match calculate_well_energies(a, b, emax) {
@@ -59,4 +67,5 @@ fn main() {
         Ok(()) => println!("Energy differences calculated successfully."),
         Err(e) => eprintln!("Error occurred: {}", e),
     }
+    
 }
