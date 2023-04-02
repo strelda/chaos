4e0b9db8e35b7f6c07078d12fcefd8997e37e45b
strelda <strelecekja@gmail.com>
2023-04-02 21:14:34 +0200

    3d well

--- quantum_chaos/src/main.rs
+++ quantum_chaos/src/main.rs
@@ -12,17 +12,14 @@ use module::{*};
 /// * `filename` - The name of the file to write the energies to
 /// ## Example:
 /// `run_2d((PI / 3.0).sqrt() , 1.0, 1e6, "generated_data/2d/energies.txt", "generated_data/2d/endif.txt");`
-fn run_2d(a: f64, b: f64, emax: f64, filename_ene: &str, filename_dif: &str) {
+fn run_2d(a: f64, b: f64, emax: f64, filename_ene: &str) {
    let well2d = Well::new(vec![a,b]);
 
    match well2d.calculate_energies(emax, filename_ene) {
       Ok(()) => println!("Energies for 2D well calculated successfully."),
       Err(e) => eprintln!("Error occurred in well2d.calculate_energies(): {}", e),
    }
-   match well2d.calculate_energy_differences(emax, filename_dif) {
-      Ok(()) => println!("Energy differences for 2D well calculated successfully."),
-      Err(e) => eprintln!("Error occurred in well2d.calculate_energy_differences(): {}", e),
-   }
+
 }
 
 /// Calculate the 3D well energies and energy differences
@@ -35,23 +32,19 @@ fn run_2d(a: f64, b: f64, emax: f64, filename_ene: &str, filename_dif: &str) {
 /// * `filename_dif` - The name of the file to write the energy differences to
 /// ## Example:
 /// `run_3d(1.0, (PI / 3.0).sqrt() , (E.powi(3)/21.0).sqrt(), 1e6, "generated_data/2d/energies.txt", "generated_data/2d/endif");`
-fn run_3d(a: f64, b: f64, c: f64, emax: f64, filename_ene: &str, filename_dif: &str) {
+fn run_3d(a: f64, b: f64, c: f64, emax: f64, filename_ene: &str) {
    let well3d = Well::new(vec![a,b,c]);
 
    match well3d.calculate_energies(emax, filename_ene) {
       Ok(()) => println!("Energies for 3D well calculated successfully."),
       Err(e) => eprintln!("Error occurred in well3d.calculate_energies(): {}", e),
    }
-   match well3d.calculate_energy_differences(emax, filename_dif) {
-      Ok(()) => println!("Energy differences for 3D well calculated successfully."),
-      Err(e) => eprintln!("Error occurred in well3d.calculate_energy_differences(): {}", e),
-   }
 }
 
 fn main() {
  
-    run_2d((PI / 3.0).sqrt() , 1.0, 1e6, "generated_data/2d/energies.txt", "generated_data/2d/endif.txt");
+    run_2d((PI / 3.0).sqrt() , 1.0, 1e6, "generated_data/2d/energies.txt");
 
-    run_3d(1.0, (PI / 3.0).sqrt() , (E.powi(3)/21.0).sqrt(), 1e5, "generated_data/3d/energies.txt", "generated_data/3d/endif.txt");
+    run_3d(1.0, (PI / 3.0).sqrt() , (E.powi(3)/21.0).sqrt(), 1e5, "generated_data/3d/energies.txt");
 
 }
--- quantum_chaos/src/plotters.py
+++ quantum_chaos/src/plotters.py
@@ -1,6 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.rcParams['text.latex.preamble'] = [
+    r'\usepackage{siunitx}',  # consistent units formatting
+    r'\sisetup{detect-all}',  # current font settings for units
+    r'\usepackage{amsmath}',  # additional math environments
+    r'\usepackage{amssymb}',  # additional math symbols
+    r'\usepackage{bm}',  # bold math symbols
+    r'\usepackage{mathrsfs}',  # math script fonts
+]
 
 
 def plot_energies(filename: str, bins: int):
@@ -9,15 +19,16 @@ def plot_energies(filename: str, bins: int):
     plt.hist(data, bins=bins)
     plt.xlabel("Energy of the well")
     plt.ylabel("Frequency")
-    plt.savefig(filename + '.png', dpi=300)
+    plt.savefig(filename + '.pdf', dpi=300)
     plt.show()
 
-
 def plot_energy_differences(filename: str, bins: int):
-    data = np.loadtxt(filename+".txt")
+    data = np.sort(np.loadtxt(filename+".txt"))
+    # differencec between neighbors
+    diff = data[1:-1]-data[0:-2]
 
-    plt.hist(data, bins=bins)
+    plt.hist(diff, bins=bins)
     plt.xlabel("S")
     plt.ylabel("Frequency")
-    plt.savefig(filename+'.png', dpi=300)
+    plt.savefig(filename+'_dif.pdf', dpi=300)
     plt.show()
